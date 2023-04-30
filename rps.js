6e5a35258230dad2fd777434bcd1fcf3ed89bcb3
Avielle Lewis <aviselanj@gmail.com>
2023-04-30 13:50:49 -0500

    added rps.js

---
+++ rps.js
@@ -0,0 +1,6 @@
+let choiceArray = ["rock", "paper", "scissors"];
+let computerChoice = Math.random(Math.floor(choiceArray.length))
+function getComputerSelection() {
+
+}
+console.log(computerChoice)
